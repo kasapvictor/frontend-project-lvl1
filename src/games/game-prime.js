24160abb3d3544697cf3f3d14984e27a4e49e7ef
kasap.victor@gmail.com <MHPFmrZq7ChXRpf>
2021-10-02 07:31:12 +0300

    fix: ESLint errors

--- src/games/game-prime.js
+++ src/games/game-prime.js
@@ -3,7 +3,7 @@ import getRandom from '../utils/random.js';
 const isPrime = (n) => {
   let prime = true;
 
-  for (let i = 2; i < n; i += 1 ) {
+  for (let i = 2; i < n; i += 1) {
     if (n % i === 0) {
       prime = false;
       break;
@@ -17,7 +17,7 @@ export default () => {
   const rules = 'Answer "yes" if given number is prime. Otherwise answer "no".\n';
   const n = getRandom();
   const question = `${n}`;
-  const answer = `${isPrime(n) ? "yes" : "no"}`;
+  const answer = `${isPrime(n) ? 'yes' : 'no'}`;
 
   return { question, answer, rules };
 };
