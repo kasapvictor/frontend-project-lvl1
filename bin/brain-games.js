e7e0c9889317a5c6631c30099ae5c5d35145bbd6
Victor Kasap <kasap.victor@gmail.com>
2021-11-07 12:42:48 +0300

    feat: moved greetings to brain-games

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,22 @@
 #!/usr/bin/env node
-import hello from '../src/cli.js';
+
+import readlineSync from 'readline-sync';
+
+const CONFIG = {
+  name: '',
+  welcome: 'Welcome to the Brain Games!',
+  askName: 'May I have your name? \n',
+  hello: () => `Hello, ${CONFIG.name}!`,
+};
+
+const hello = () => {
+  console.log(CONFIG.welcome);
+
+  const userName = readlineSync.question(CONFIG.askName);
+
+  CONFIG.name = userName !== '' ? userName : 'John Doe';
+
+  console.log(CONFIG.hello());
+};
 
 hello();
