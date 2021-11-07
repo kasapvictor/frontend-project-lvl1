dda1ce7768c31053ca261c2c9cd346f9b5bc1a50
Victor Kasap <victorkasap@Galactus.local>
2021-11-07 08:52:33 +0300

    feat: update cli greeteng text

--- src/cli.js
+++ src/cli.js
@@ -2,9 +2,15 @@ import readlineSync from 'readline-sync';
 import CONFIG from './utils/config.js';
 
 const hello = () => {
-  console.log(CONFIG.greeting);
-  CONFIG.name = readlineSync.question(CONFIG.askName);
-  console.log(CONFIG.hello());
+  const WELCOME_TEXT = 'Welcome to the Brain Games!';
+  const ASK_NAME = 'May I have your name? \n';
+  const HELLO_TEXT = (name) => `Hello, ${name}!`;
+
+  console.log(WELCOME_TEXT);
+
+  CONFIG.name = readlineSync.question(ASK_NAME);
+
+  console.log(HELLO_TEXT(CONFIG.name));
 };
 
 export default hello;
--- src/utils/config.js
+++ src/utils/config.js
@@ -1,11 +1,8 @@
 const CONFIG = {
   game: null,
-  greeting: 'Welcome to the Brain Games!',
-  askName: 'May I have your name? \n',
   askAnswer: 'Your answer: ',
   name: '',
   question: (question) => (`Question: ${question}`),
-  hello: () => `Hello, ${CONFIG.name}!`,
   correctText: 'Correct!',
   incorrectText: (userAnswer, expectedAnswer) => `'${userAnswer}' is wrong answer ;(. Correct answer was '${expectedAnswer}'.`,
   winText: () => `Congratulations, ${CONFIG.name}!`,
