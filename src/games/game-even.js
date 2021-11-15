52d515c94fe1d9ad341aa0f2547542e3266c16ac
Victor Kasap <kasap.victor@gmail.com>
2021-11-15 06:41:01 +0300

    fix: .DS_Store

--- src/games/game-even.js
+++ src/games/game-even.js
@@ -3,11 +3,11 @@ import start from '../index.js';
 
 const RULES = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-const isEven = (n) => (n % 2 === 0 ? 'yes' : 'no');
+const isEven = (n) => (n % 2 === 0);
 
 const game = () => {
   const question = getRandom();
-  const expectedAnswer = isEven(question);
+  const expectedAnswer = isEven(question) ? 'yes' : 'no';
 
   return [question, expectedAnswer];
 };
